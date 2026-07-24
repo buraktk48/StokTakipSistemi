df89a5959203d71f97e2f86993e21a090469ecbf
owenspecter <evinimo21@gmail.com>
2026-07-24 16:04:15 +0300

    Add project files.

---
+++ StokTakipSistemi/Entities/Rol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Entities
+{
+    internal class Rol
+    {
+        public int Id { get; set; }
+        public string adi { get; set; }
+
+        public DateTime olusturulma_zamani { get; set; }
+    }
+}
---
+++ StokTakipSistemi/Entities/Urun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Entities
+{
+    internal class Urun
+    {
+        public int id { get; set; }
+        public int urun_kodu { get; set; }
+        public string urun_adi { get; set; }
+        public string birim { get; set; }
+
+        public DateTime olusturulma_zamani { get; set; }
+
+
+    }
+}
